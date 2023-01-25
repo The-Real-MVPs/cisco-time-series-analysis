b6320cb0a36decd982f8091772cce377e7fd4042
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-01-25 15:14:46 -0600

    add orders

--- src/summary.py
+++ src/summary.py
@@ -2,26 +2,4 @@ import pandas as pd
 import numpy as np
 
 
-def get_summary_orders_df(df):
-    '''
-    Groups by order date, customer name, customer type, customer city and reseller name.
-    Calculates the purchase amount by the end. of the day
-    
-    Parameters:
-        df: pandas data frame with the data pulled from DIR site
-    Returns:
-        pd.DataFrame with the sum of the purchase amount by the end of the day for every company
-    '''
-    summary_df = pd.DataFrame()
-    if 'order_date_copy' in df.columns:
-        summary_df = df.groupby(by=\
-                        ['order_date_copy', 'customer_name', 'customer_type', 'customer_city', 'reseller_name', 'shipped_date'])\
-                        .order_quantity.sum().to_frame().reset_index()
-        summary_df.rename(columns={'order_date_copy':'order_date'}, inplace=True)
-        summary_df = summary_df.set_index('order_date').sort_index()
-    elif 'order_date' in df.columns:
-        summary_df = df.groupby(by=\
-                        ['order_date', 'customer_name', 'customer_type', 'customer_city', 'reseller_name', 'shipped_date'])\
-                        .purchase_amount.sum().to_frame().reset_index()
-        summary_df = summary_df.set_index('order_date').sort_index()  
-    return summary_df
+c
